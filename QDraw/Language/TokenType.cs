d389c7db8d35943621b697eae9d510d3b958a692
vo1x <ak2061@icloud.com>
2025-10-31 23:17:02 +0545

    feat: implement token types and token class

---
+++ QDraw/Language/TokenType.cs
@@ -0,0 +1,19 @@
+public enum TokenType
+{
+    // Keywords
+    LINE,
+    STOP,
+    
+    // Symbols
+    LPAREN,
+    RPAREN,
+    COMMA,
+    
+    // Literals
+    NUMBER,
+    IDENTIFIER,
+    
+    // Meta
+    EOF
+    
+}
